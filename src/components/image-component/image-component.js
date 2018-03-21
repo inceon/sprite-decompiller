d03d36fac913ad2f633b9c0bc3efa788a5b71ac8
Sergey Okhrymenko <mr.inceon@gmail.com>
2018-03-21 23:24:25 +0200

    added canvas image drawing

--- src/components/image-component/image-component.js
+++ src/components/image-component/image-component.js
@@ -3,10 +3,9 @@ import Dropzone from 'react-dropzone'
 import styles from './image-component.scss';
 
 export default class ImageComponent extends Component {
-    constructor() {
-        super();
-        this.state = {files: []}
-    }
+    state = {
+        files: []
+    };
 
     onDrop(files) {
         var reader = new FileReader();
@@ -18,9 +17,33 @@ export default class ImageComponent extends Component {
         reader.readAsDataURL(files[0]);
     }
 
+    componentDidUpdate() {
+        if (this.refs.canvas) {
+            const canvas = this.refs.canvas;
+            canvas.width = this.refs[styles['image-component']].offsetWidth;
+            canvas.height = this.refs[styles['image-component']].offsetHeight;
+            const ctx = canvas.getContext('2d');
+
+            let image = new Image();
+            image.onload = function () {
+
+                let widthScale = canvas.width / image.width;
+                let heightScale = canvas.height / image.height;
+
+                if (widthScale < heightScale) {
+                    ctx.drawImage(image, 0, 0, canvas.width, image.height * widthScale);
+                } else {
+                    ctx.drawImage(image, 0, 0, image.width * heightScale, canvas.height);
+                }
+
+            };
+            image.src = this.state.readedFile;
+        }
+    }
+
     render() {
         return (
-            <div id={styles['image-component']}>
+            <div id={styles['image-component']} ref={styles['image-component']}>
                 {!this.state.readedFile && (
                     <Dropzone className={styles['drop-zone']}
                               onDrop={this.onDrop.bind(this)}
@@ -30,7 +53,8 @@ export default class ImageComponent extends Component {
                     </Dropzone>
                 )}
                 {this.state.readedFile && (
-                    <img className={styles.image} src={this.state.readedFile} alt="Red dot"/>
+                    <canvas ref='canvas' className={styles.canvas}/>
+                    /*<img className={styles.image} src={this.state.readedFile} alt="Red dot"/>*/
                 )}
             </div>
         );
