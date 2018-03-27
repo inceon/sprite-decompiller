38f501ffdc5dc03dd7afaf36dc2ee24ea430d896
Sergey Okhrymenko <mr.inceon@gmail.com>
2018-03-27 23:18:58 +0300

    update webpack prod config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -71,7 +71,7 @@ module.exports = {
         devtoolModuleFilenameTemplate: info =>
             path
                 .relative(paths.appSrc, info.absoluteResourcePath)
-                .replace(/\\/g, '/'),
+                .replace(/\\/g, '/')
     },
     resolve: {
         // This allows you to set a fallback for where Webpack should look for modules.
@@ -93,7 +93,7 @@ module.exports = {
 
             // Support React Native Web
             // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-            'react-native': 'react-native-web',
+            'react-native': 'react-native-web'
         },
         plugins: [
             // Prevents users from importing files from outside of src/ (or node_modules/).
@@ -101,8 +101,8 @@ module.exports = {
             // To fix this, we prevent you from importing files out of src/ -- if you'd like to,
             // please link the files into your node_modules/ and let module-resolution kick in.
             // Make sure your source files are compiled, as they will not be processed in any way.
-            new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
-        ],
+            new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson])
+        ]
     },
     module: {
         strictExportPresence: true,
@@ -120,13 +120,13 @@ module.exports = {
                     {
                         options: {
                             formatter: eslintFormatter,
-                            eslintPath: require.resolve('eslint'),
+                            eslintPath: require.resolve('eslint')
 
                         },
-                        loader: require.resolve('eslint-loader'),
-                    },
+                        loader: require.resolve('eslint-loader')
+                    }
                 ],
-                include: paths.appSrc,
+                include: paths.appSrc
             },
             {
                 // "oneOf" will traverse all following loaders until one will
@@ -140,8 +140,8 @@ module.exports = {
                         loader: require.resolve('url-loader'),
                         options: {
                             limit: 10000,
-                            name: 'static/media/[name].[hash:8].[ext]',
-                        },
+                            name: 'static/media/[name].[hash:8].[ext]'
+                        }
                     },
                     // Process JS with Babel.
                     {
@@ -150,8 +150,8 @@ module.exports = {
                         loader: require.resolve('babel-loader'),
                         options: {
 
-                            compact: true,
-                        },
+                            compact: true
+                        }
                     },
                     // The notation here is somewhat confusing.
                     // "postcss" loader applies autoprefixer to our CSS.
@@ -167,50 +167,37 @@ module.exports = {
                     // in the main CSS file.
                     {
                         test: /\.css$/,
-                        loader: ExtractTextPlugin.extract(
-                            Object.assign(
+                        use: ExtractTextPlugin.extract({
+                            fallback: 'style-loader',
+                            use: [
                                 {
-                                    fallback: {
-                                        loader: require.resolve('style-loader'),
-                                        options: {
-                                            hmr: false,
-                                        },
-                                    },
-                                    use: [
-                                        {
-                                            loader: require.resolve('css-loader'),
-                                            options: {
-                                                importLoaders: 1,
-                                                minimize: true,
-                                                sourceMap: shouldUseSourceMap,
-                                            },
-                                        },
-                                        {
-                                            loader: require.resolve('postcss-loader'),
-                                            options: {
-                                                // Necessary for external CSS imports to work
-                                                // https://github.com/facebookincubator/create-react-app/issues/2677
-                                                ident: 'postcss',
-                                                plugins: () => [
-                                                    require('postcss-flexbugs-fixes'),
-                                                    autoprefixer({
-                                                        browsers: [
-                                                            '>1%',
-                                                            'last 4 versions',
-                                                            'Firefox ESR',
-                                                            'not ie < 9', // React doesn't support IE8 anyway
-                                                        ],
-                                                        flexbox: 'no-2009',
-                                                    }),
-                                                ],
-                                            },
-                                        },
-                                    ],
+                                    loader: 'css-loader',
+                                    options: {
+                                        modules: true,
+                                        localIdentName: '[name]__[local]__[hash:base64:5]'
+                                    }
                                 },
-                                extractTextPluginOptions
-                            )
-                        ),
-                        // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
+                                'postcss-loader'
+                            ]
+                        })
+                    },
+                    {
+                        test: /\.scss$/,
+                        use: ExtractTextPlugin.extract({
+                            fallback: 'style-loader',
+                            use: [
+                                {
+                                    loader: 'css-loader',
+                                    options: {
+                                        modules: true,
+                                        sourceMap: true,
+                                        importLoaders: 2,
+                                        localIdentName: '[name]__[local]__[hash:base64:5]'
+                                    }
+                                },
+                                'sass-loader'
+                            ]
+                        })
                     },
                     // "file" loader makes sure assets end up in the `build` folder.
                     // When you `import` an asset, you get its filename.
@@ -222,18 +209,28 @@ module.exports = {
                         // it's runtime that would otherwise processed through "file" loader.
                         // Also exclude `html` and `json` extensions so they get processed
                         // by webpacks internal loaders.
-                        exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+                        exclude: [
+                            /\.(js|jsx|mjs)$/,
+                            /\.html$/,
+                            /\.scss$/,
+                            /\.json$/
+                        ],
                         options: {
-                            name: 'static/media/[name].[hash:8].[ext]',
-                        },
-                    },
+                            name: 'static/media/[name].[hash:8].[ext]'
+                        }
+                    }
                     // ** STOP ** Are you adding a new loader?
                     // Make sure to add the new loader(s) before the "file" loader.
-                ],
-            },
-        ],
+                ]
+            }
+        ]
     },
     plugins: [
+        new ExtractTextPlugin({
+            filename: 'styles.css',
+            allChunks: true,
+            disable: process.env.NODE_ENV !== 'production'
+        }),
         // Makes some environment variables available in index.html.
         // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
         // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
@@ -254,8 +251,8 @@ module.exports = {
                 keepClosingSlash: true,
                 minifyJS: true,
                 minifyCSS: true,
-                minifyURLs: true,
-            },
+                minifyURLs: true
+            }
         }),
         // Makes some environment variables available to the JS code, for example:
         // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
@@ -270,28 +267,28 @@ module.exports = {
                 // https://github.com/facebookincubator/create-react-app/issues/2376
                 // Pending further investigation:
                 // https://github.com/mishoo/UglifyJS2/issues/2011
-                comparisons: false,
+                comparisons: false
             },
             mangle: {
-                safari10: true,
+                safari10: true
             },
             output: {
                 comments: false,
                 // Turned on because emoji and regex is not minified properly using default
                 // https://github.com/facebookincubator/create-react-app/issues/2488
-                ascii_only: true,
+                ascii_only: true
             },
-            sourceMap: shouldUseSourceMap,
+            sourceMap: shouldUseSourceMap
         }),
         // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
         new ExtractTextPlugin({
-            filename: cssFilename,
+            filename: cssFilename
         }),
         // Generate a manifest file which contains a mapping of all asset filenames
         // to their corresponding output file so that tools can pick it up without
         // having to parse `index.html`.
         new ManifestPlugin({
-            fileName: 'asset-manifest.json',
+            fileName: 'asset-manifest.json'
         }),
         // Generate a service worker script that will precache, and keep up to date,
         // the HTML & assets that are part of the Webpack build.
@@ -321,14 +318,14 @@ module.exports = {
             // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
             navigateFallbackWhitelist: [/^(?!\/__).*/],
             // Don't precache sourcemaps (they're large) and build asset manifest:
-            staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+            staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/]
         }),
         // Moment.js is an extremely popular library that bundles large locale files
         // by default due to how Webpack interprets its code. This is a practical
         // solution that requires the user to opt into importing specific locales.
         // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
         // You can remove this if you don't use Moment.js:
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
     ],
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
@@ -337,6 +334,6 @@ module.exports = {
         fs: 'empty',
         net: 'empty',
         tls: 'empty',
-        child_process: 'empty',
-    },
+        child_process: 'empty'
+    }
 };
